2a20d2c8fa089cad7e63d9e3af122b65389aae23
Deeptech <deeptech104@gmail.com>
2024-12-31 09:48:54 +0500

    Add files via upload

---
+++ lecture0.js
@@ -0,0 +1,2 @@
+//console.log("Hello World") is u
+console.log("welcome to DeepTech")
---
+++ variable.js
@@ -0,0 +1,8 @@
+var id = 104;
+id = 105;
+let name = "john"
+name = "peter ";
+
+const student= "deepk";
+student='sachin';// error
+console.table({id,name,student});
